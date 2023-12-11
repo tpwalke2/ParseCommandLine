688ffcd1dbd5aaf4ec8c8800e37fc260741e4e59
Tom Walker <tpwalke2@gmail.com>
2023-12-11 06:45:38 -0500

    Fix code inspection problems

--- ParseCommandLine.Core/Serialization/Json.cs
+++ ParseCommandLine.Core/Serialization/Json.cs
@@ -5,18 +5,20 @@ namespace ParseCommandLine.Core.Serialization;
 
 public static class Json
 {
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        DictionaryKeyPolicy  = JsonNamingPolicy.CamelCase,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Converters =
+        {
+            new JsonStringEnumConverter()
+        }
+    };
+    
     public static T? Deserialize<T>(string value)
     {
         return JsonSerializer.Deserialize<T>(
             value,
-            new JsonSerializerOptions
-            {
-                DictionaryKeyPolicy  = JsonNamingPolicy.CamelCase,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                Converters =
-                {
-                    new JsonStringEnumConverter()
-                }
-            });
+            Options);
     }
 }
